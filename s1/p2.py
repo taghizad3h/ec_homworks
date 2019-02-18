08eb69b0bbf6c2e7c0653989af1c3f9b14247ca6
Ali Asghar Taghizadeh <tagizaade@gmail.com>
2019-02-18 09:57:12 +0330

    nothing

--- s1/p2.py
+++ s1/p2.py
@@ -5,14 +5,15 @@ logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:
 
 
 ## settings
-n_dims = 5000
+n_dims = 1000
 cost_func = rosen
 iterations = 1000
 consider_cov = False
 
 es = ES(n_dims, cost_func)
 es.evolve(iterations=iterations, consider_cov=consider_cov)
-es.plot_history()
+# es.plot_history()
+es.save_history('second_model_cov_rosen_1000dim')
 
 
 
@@ -23,4 +24,5 @@ consider_cov = False
 
 es = ES(n_dims, cost_func)
 es.evolve(iterations=iterations, consider_cov=consider_cov)
-es.plot_history()
+# es.plot_history()
+es.save_history('second_model_cov_ackely_1000dim')
