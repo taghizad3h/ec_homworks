f1a8db97251d3f5da51969de8a52636188ab4e75
Ali Asghar Taghizadeh <tagizaade@gmail.com>
2019-02-19 20:04:44 +0330

    improved gp

--- s2/p1.py
+++ s2/p1.py
@@ -8,44 +8,58 @@ from data_helper import get_first_dataset
 from gprs import GPRegressionSolver
 from tools import draw
 from es import ES
+from sklearn.model_selection import train_test_split
+
+
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
 
 ################  config  ###############
 
 # random.seed(318)
-constants = [0.1, 1.1]
+constants = [-1.1, 0.1, 1.1]
 data_path = 'data/Concrete_Data.csv'
 population_size = 300
 verbose = True
 mating_prob = 0.5
 mutating_prob = 0.1
-n_generations = 1000
+n_generations = 1
 parsimonyPressure = False
 parsimonyW1 = 0.01
 parsimonyW2 = 0.99
 tournsize = 3
+min_height=1
+max_height=5
+mut_min_height=0
+mut_max_height=4
 
 
 ############ preparing data #############
 X, y, var_names = get_first_dataset(data_path)
+
 constants = np.array(constants, dtype=float)
 if len(var_names)>0:
     for i in range(len(constants)):
         var_names.append('constant_'+str(i))
 
 
-
 ########### running algorithm ###########
 gpsolver = GPRegressionSolver(X, y, constants=constants, var_names=var_names, 
-    use_parsimony_pressure=parsimonyPressure, parsimony_pressure_w1=parsimonyW1, parsimony_pressure_w2=parsimonyW2)
+    use_parsimony_pressure=parsimonyPressure, parsimony_pressure_w1=parsimonyW1, parsimony_pressure_w2=parsimonyW2
+    , tourn_size=tournsize, min_height=min_height, max_height=max_height, mut_min_height=mut_min_height
+    , mut_max_height=mut_max_height)
+
 last_pop, log = gpsolver.solve(n_generations=n_generations)
+best_fitness = log.chapters['fitness'].select('min')
+avg_size = log.chapters['size'].select('avg')
+np.savetxt('best_fits.txt', best_fitness)
+np.savetxt('avg_size', avg_size)
 
 
 ######## saving best solution found #########
 best = max(last_pop, key=operator.attrgetter("fitness"))
 logging.info('best individual\'s fittness: '+ str(best.fitness))
 draw(best, 'out/best_ind.pdf')
-
 
 
 ######### using evolution strategy ##########
@@ -56,5 +70,5 @@ def my_cost(constants):
 logging.info('current constatns '+str(constants))
 mes = ES(len(constants), my_cost)
 mes.set_ans(constants)
-mes.evolve()
+mes.evolve(iterations = 1)
 logging.info('best found constants '+str(mes.ans))
